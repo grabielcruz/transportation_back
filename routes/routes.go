ef3b3c03a89ae47f47349a23bbe09cfafde28536
Luis G. Cruz <luisgrabielcruz@gmail.com>
2023-01-26 21:39:38 -0400

    updating routes on main

--- routes/routes.go
+++ routes/routes.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/grabielcruz/transportation_back/modules/bills"
+	"github.com/grabielcruz/transportation_back/modules/currencies"
 	"github.com/grabielcruz/transportation_back/modules/money_accounts"
+	"github.com/grabielcruz/transportation_back/modules/person_accounts"
 	"github.com/grabielcruz/transportation_back/modules/persons"
 	"github.com/grabielcruz/transportation_back/modules/transactions"
 	"github.com/julienschmidt/httprouter"
@@ -20,8 +23,11 @@ func SetupAndGetRoutes() *httprouter.Router {
 
 	router.GET("/ping", InitialHandler)
 
+	currencies.Routes(router)
 	money_accounts.Routes(router)
 	persons.Routes(router)
+	person_accounts.Routes(router)
+	bills.Routes(router)
 	transactions.Routes(router)
 
 	return router
